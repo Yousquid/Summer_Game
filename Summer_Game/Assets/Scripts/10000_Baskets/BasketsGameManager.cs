b01f80485c00a616965e3e785405a7c7c9b04620
Yousquid <ly2456@nyu.edu>
2025-09-22 09:22:44 +0800

    QUICK_SAVE

--- Summer_Game/Assets/Scripts/10000_Baskets/BasketsGameManager.cs
+++ Summer_Game/Assets/Scripts/10000_Baskets/BasketsGameManager.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class BasketsGameManager : MonoBehaviour
 {
@@ -6,7 +10,13 @@ public class BasketsGameManager : MonoBehaviour
 
     public gameStage currentStage;
 
+    [System.Serializable]
+    public class Texts
+    {
+        public List<string> texts;
+    }
 
+    public List<Texts> textsList;
     public static int day = 1;
     void Start()
     {
---
+++ Summer_Game/Assets/Scripts/10000_Baskets/MealEat.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class MealEat : MonoBehaviour
+{
+    public Sprite eat_once;
+    public Sprite eat_twice;
+    private SpriteRenderer sprite;
+    private bool canEat = false;
+    private int leftEatTimes = 3;
+
+    private void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "BasketEnd")
+        {
+            
+        }
+        if (collision.tag == "Light")
+        {
+            
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Light")
+        {
+            canEat = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Light")
+        {
+            canEat = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (canEat)
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                leftEatTimes -= 1;
+            }
+        }
+
+        if (leftEatTimes == 2)
+        {
+            sprite.sprite = eat_once;
+        }
+        if (leftEatTimes == 1)
+        {
+            sprite.sprite = eat_twice;
+        }
+    }
+
+}
